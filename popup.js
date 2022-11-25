547f728b8eda6ecfbcd91348cc0275b441578d53
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2022-11-26 02:10:44 +0530

    Add files via upload
    Added basic html css and js files for basic layout of extension

---
+++ popup.js
@@ -0,0 +1,11 @@
+// popup page
+
+var b = document.getElementById("rate");
+b.onclick = ()=>{
+  window.open('https://www.linkedin.com/in/ishan-audichya-a929b4236/')
+}
+
+// var copy = document.getElementById("copy");
+// copy.onclick = ()=>{
+//   navigator.clipboard.writeText("");
+// }
